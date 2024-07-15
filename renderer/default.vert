fc455c444a2552a0a79c5ba8ec61ea157ee92f60
Tristan Lassiter <tristan.lassi@gmail.com>
2024-07-15 00:20:30 -0600

    add camera, texture support, glm

--- renderer/default.vert
+++ renderer/default.vert
@@ -4,18 +4,22 @@
 layout (location = 0) in vec3 aPos;
 // Colors
 layout (location = 1) in vec3 aColor;
-
+// Coordinate
+layout (location = 2) in vec2 aTex;
 
 // Outputs the color for the Fragment Shader
 out vec3 color;
-// Controls the scale of the vertices
-uniform float scale;
 
+// Texture Coordinates
+out vec2 texCoord;
+
+uniform mat4 camMatrix;
 
 void main()
 {
 	// Outputs the positions/coordinates of all vertices
-	gl_Position = vec4(aPos.x + aPos.x * scale, aPos.y + aPos.y * scale, aPos.z + aPos.z * scale, 1.0);
+	gl_Position = camMatrix * vec4(aPos,1.0f);
 	// Assigns the colors from the Vertex Data to "color"
 	color = aColor;
+	texCoord = aTex;
 }
--- renderer/main.cpp
+++ renderer/main.cpp
@@ -1,24 +1,30 @@
 #include<iostream>
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
+#include<stb/stb_image.h>
+#include<glm/glm.hpp>
+#include<glm/gtc/matrix_transform.hpp>
+#include<glm/gtc/type_ptr.hpp>
 
 #include"shaderClass.h"
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
+#include"Camera.h"
 
-
+const unsigned int width = 800;
+const unsigned int height = 800;
 
 // Vertices coordinates
 float vertices[] = {
-		0.5f,  0.5f, 0.0f,  1.0f,0.0f,0.0f,// top right
-		0.5f, -0.5f, 0.0f,  0.0f,1.0f,0.0f,// bottom right
-	   -0.5f, -0.5f, 0.0f,  0.0f,0.0f,1.0f,// bottom left
-	   -0.5f,  0.5f, 0.0f,   0.0f,1.0f,0.0f,// top left 
+	   -0.5f, -0.5f, 0.0f,  1.0f,0.0f,0.0f,  0.0f,0.0f,// lf
+	   -0.5f,  0.5f, 0.0f,  0.0f,1.0f,0.0f,  0.0f,1.0f,// ul
+	    0.5f,  0.5f, 0.0f,  0.0f,0.0f,1.0f,  1.0f,1.0f,// ur
+	    0.5f, -0.5f, 0.0f,  1.0f,1.0f,0.0f,  1.0f,0.0f,// ll
 };
 unsigned int indices[] = {  // note that we start from 0!
-	0, 1, 3,  // first Triangle
-	1, 2, 3   // second Triangle
+	0, 1, 2,  // first Triangle
+	0, 2, 3   // second Triangle
 };
 
 int main()
@@ -34,8 +40,8 @@ int main()
 	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
+	// Create a GLFWwindow object of 800 by 800 pixels
+	GLFWwindow* window = glfwCreateWindow(width, height, "OpenGL", NULL, NULL);
 	// Error check if the window fails to create
 	if (window == NULL)
 	{
@@ -50,14 +56,10 @@ int main()
 	gladLoadGL();
 	// Specify the viewport of OpenGL in the Window
 	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
-
-
+	glViewport(0, 0, width, height);
 
 	// Generates Shader object using shaders defualt.vert and default.frag
 	Shader shaderProgram("default.vert", "default.frag");
-
-
 
 	// Generates Vertex Array Object and binds it
 	VAO VAO1;
@@ -69,8 +71,10 @@ int main()
 	EBO EBO1(indices, sizeof(indices));
 
 	// Links VBO attributes such as coordinates and colors to VAO
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+
 	// Unbind all to prevent accidentally modifying them
 	VAO1.Unbind();
 	VBO1.Unbind();
@@ -79,6 +83,30 @@ int main()
 	// Gets ID of uniform called "scale"
 	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
+	//Texture
+	int widthImg, heightImg, numColch;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* bytes = stbi_load("checker.jpg", &widthImg, &heightImg, &numColch, 0);
+
+	GLuint texture;
+	glGenTextures(1, &texture);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, widthImg, heightImg, 0, GL_RGB, GL_UNSIGNED_BYTE, bytes);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	stbi_image_free(bytes);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	
+	GLuint tex0Uni = glGetUniformLocation(shaderProgram.ID, "tex0");
+	shaderProgram.Activate();
+	glUniform1f(tex0Uni, 0);
+
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
@@ -89,8 +117,11 @@ int main()
 		glClear(GL_COLOR_BUFFER_BIT);
 		// Tell OpenGL which Shader Program we want to use
 		shaderProgram.Activate();
-		// Assigns a value to the uniform; NOTE: Must always be done after activating the Shader Program
-		glUniform1f(uniID, 0.5f);
+
+		camera.Matrix(45.0f, .1f, 100.0f, shaderProgram, "camMatrix");
+
+		// Tex coordinates
+		glBindTexture(GL_TEXTURE_2D, texture);
 		// Bind the VAO so OpenGL knows to use it
 		VAO1.Bind();
 		// Draw primitives, number of indices, datatype of indices, index of indices
@@ -107,6 +138,7 @@ int main()
 	VAO1.Delete();
 	VBO1.Delete();
 	EBO1.Delete();
+	glDeleteTextures(1, &texture);
 	shaderProgram.Delete();
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
