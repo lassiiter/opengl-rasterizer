a5e7f89ca35bda44eaf731dfd4388fc5cd91b593
Tristan Lassiter <tristan.lassi@gmail.com>
2024-12-30 17:13:45 -0700

    mostly done approximate roughness

--- opengl-rasterizer/shaders/pbr.fs
+++ opengl-rasterizer/shaders/pbr.fs
@@ -11,9 +11,13 @@ uniform float u_metallic;
 uniform float u_roughness;
 uniform float u_ao;
 
-layout(location = 3) uniform sampler2D albedoTex;
-layout(location = 4) uniform sampler2D ormTex;
-layout(location = 7) uniform sampler2D irradianceTex;
+layout(binding  = 3) uniform sampler2D albedoTex;
+layout(binding  = 4) uniform sampler2D ormTex;
+layout(binding  = 5) uniform sampler2D emissiveTex;
+
+layout(binding  = 7) uniform sampler2D irradianceTex;
+layout(binding  = 8) uniform sampler2D radianceTex;
+
 //uniform sampler2D normal;
 
 // lights
@@ -23,6 +27,7 @@ uniform vec3 lightColor;
 uniform vec3 camPos;
 
 const float PI = 3.14159265359;
+const float MAX_REFLECTION_LOD = 4.0;
 // ----------------------------------------------------------------------------
 float DistributionGGX(vec3 N, vec3 H, float roughness)
 {
@@ -63,6 +68,10 @@ vec3 fresnelSchlick(float cosTheta, vec3 F0)
 {
   return F0 + (1.0 - F0) * pow(max(1.0 - cosTheta, 0.0), 5.0);
 }
+vec3 fresnelSchlickRoughness(float cosTheta, vec3 F0, float roughness)
+{
+    return F0 + (max(vec3(1.0 - roughness), F0) - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+} 
 // ----------------------------------------------------------------------------
 void main()
 {
@@ -77,10 +86,12 @@ void main()
   vec3 irradiance = texture(irradianceTex, vec2(phi, theta)).rgb;
   vec3 albedo = pow(texture(albedoTex, TexCoords).rgb, vec3(2.2));
   vec3 orm    = (texture(ormTex, TexCoords)).rgb;
+  vec3 emissive    = (texture(emissiveTex, TexCoords)).rgb;
 
   float ao = orm.r;
-  float metallic = orm.g;
   float roughness = orm.b;
+  float metallic = orm.g;
+  vec3 specular = textureLod(radianceTex, vec2(phi, theta),roughness * MAX_REFLECTION_LOD).rgb;
 
   // calculate reflectance at normal incidence; if dia-electric (like plastic) use F0 
   // of 0.04 and if it's a metal, use the albedo color as F0 (metallic workflow)    
@@ -89,45 +100,52 @@ void main()
 
   // reflectance equation
   vec3 Lo = vec3(0.0);
+  for(int i = 0; i < 1; ++i) 
+  {
+      // calculate per-light radiance
+      vec3 L = normalize(lightPosition - WorldPos);
+      vec3 H = normalize(V + L);
+      float distance = length(lightPosition - WorldPos);
+      float attenuation = 1.0 / (distance * distance);
+      vec3 radiance = lightColor * attenuation;
 
-  // calculate per-light radiance
-  vec3 L = normalize(lightPosition - WorldPos);
-  vec3 H = normalize(V + L);
-  float distance = length(lightPosition - WorldPos);
-  float attenuation = 1.0 / (distance * distance);
-  vec3 radiance = lightColor * attenuation;
+      // Cook-Torrance BRDF
+      float NDF = DistributionGGX(N, H, roughness);
+      float G = GeometrySmith(N, V, L, roughness);
+      vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);  
 
-  // Cook-Torrance BRDF
-  float NDF = DistributionGGX(N, H, roughness);
-  float G = GeometrySmith(N, V, L, roughness);
-  vec3 F = fresnelSchlick(clamp(dot(H, V), 0.0, 1.0), F0);
+      vec3 nominator = NDF * G * F;
+      float denominator = 4 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
+      vec3 specular_Lo = nominator / max(denominator, 0.001); // prevent divide by zero for NdotV=0.0 or NdotL=0.0
+     
+      // kS is equal to Fresnel
+      vec3 kS = F;
+      // for energy conservation, the diffuse and specular light can't
+      // be above 1.0 (unless the surface emits light); to preserve this
+      // relationship the diffuse component (kD) should equal 1.0 - kS.
+      vec3 kD = vec3(1.0) - kS;
+      // multiply kD by the inverse metalness such that only non-metals 
+      // have diffuse lighting, or a linear blend if partly metal (pure metals
+      // have no diffuse light).
+      kD *= 1.0 - metallic;
 
-  vec3 nominator = NDF * G * F;
-  float denominator = 4 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0);
-  vec3 specular = nominator / max(denominator, 0.001); // prevent divide by zero for NdotV=0.0 or NdotL=0.0
+      // scale light by NdotL
+      float NdotL = max(dot(N, L), 0.0);
 
-  // kS is equal to Fresnel
-  vec3 kS = F;
-  // for energy conservation, the diffuse and specular light can't
-  // be above 1.0 (unless the surface emits light); to preserve this
-  // relationship the diffuse component (kD) should equal 1.0 - kS.
-  vec3 kD = vec3(1.0) - kS;
-  // multiply kD by the inverse metalness such that only non-metals 
-  // have diffuse lighting, or a linear blend if partly metal (pure metals
-  // have no diffuse light).
-  kD *= 1.0 - metallic;
+      // add to outgoing radiance Lo
+      Lo += (kD * albedo / PI + specular_Lo) * radiance * NdotL;  // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
+  }
+   vec3 F = fresnelSchlickRoughness(max(dot(N, V), 0.0), F0, roughness);
 
-  // scale light by NdotL
-  float NdotL = max(dot(N, L), 0.0);
+   vec3 kS = F;
+   vec3 kD = vec3(1.0) - kS;
+   kD *= 1.0 - metallic;
 
-  // add to outgoing radiance Lo
-  Lo += (kD * albedo / PI + specular) * radiance * NdotL;  // note that we already multiplied the BRDF by the Fresnel (kS) so we won't multiply by kS again
-
-  // ambient lighting (note that the next IBL tutorial will replace 
   // this ambient lighting with environment lighting).
-  vec3 ambient = irradiance* ao;
+  vec3 diffuse      = irradiance * albedo;
+  vec3 ambient = (kD * diffuse + (specular*roughness)) * ao;
 
-  vec3 color = ambient + Lo;
+  vec3 color = ambient + Lo + (emissive*20.0);
 
   // HDR tonemapping
   color = color / (color + vec3(1.0));
@@ -135,5 +153,5 @@ void main()
   // gamma correct
   color = pow(color, vec3(1.0 / 2.2));
 
-  FragColor = vec4(albedo, 1.0);
+  FragColor = vec4(color, 1.0);
 }
--- opengl-rasterizer/source/elems/light.h
+++ opengl-rasterizer/source/elems/light.h
@@ -14,7 +14,7 @@ namespace nelems
     Light()
     {
       mColor = glm::vec3(1.0f, 1.0f, 1.0f);
-      mPosition = { 1.5f, 3.5f, 3.0f };
+      mPosition = { 100.0f, 3.5f, 3.0f };
       mStrength = 100.0f;
     }
 
